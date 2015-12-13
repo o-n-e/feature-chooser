15ab84a3e1f0f2114d52f839b7f63c75bcb324fb
o-n-e <stewart.barron@mac.com>
2015-12-13 22:17:32 +0000

    updated checkout

--- scripts/private/test-all.sh
+++ scripts/private/test-all.sh
@@ -27,7 +27,7 @@ WEBSERVER_PID=$!
 # Run the unit and e2e tests
 for i in {0..12}
 do
-  git checkout -f step-$i
+  git checkout #-f  step-$i
 
   node_modules/karma/bin/karma start test/karma.conf.js --single-run
   node_modules/.bin/protractor test/protractor-conf.js
