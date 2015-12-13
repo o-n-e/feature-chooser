046c32730d208fa2d8d9ef5e54d80769d4821f13
o-n-e <stewart.barron@mac.com>
2015-12-13 23:48:44 +0000

    3. Make the list dynamic

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,10 +2,15 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('FeatureCtrl', function(){
 
-  it("should do something", function() {
+  beforeEach(module('featureChooserApp'));
 
-  });
+  it('should create "availibleFeatures" model with 6 features', inject(function($controller) {
+    var scope = {},
+        ctrl = $controller('FeatureCtrl', {$scope:scope});
+
+    expect(scope.availibleFeatures.length).toBe(6);
+  }));
 
 });
