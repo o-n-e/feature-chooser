e3a15ec41997e6fb73ffb98ca4e4f0e78e33f56e
o-n-e <stewart.barron@mac.com>
2015-12-14 00:27:35 +0000

    4. The app now has a search box. Notice that the phone list on the page changes depending on what a user types into the search box.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,25 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('featureChooser', function() {
 
   beforeEach(function() {
     browser.get('app/index.html');
+  });
+
+  it('should filter the available features list as a user types into the search box', function() {
+
+    var availableFeaturesList = element.all(by.repeater('feature in availibleFeatures'));
+    var query = element(by.model('query'));
+
+    expect(availableFeaturesList.count()).toBe(6);
+
+    query.sendKeys('col');
+    expect(availableFeaturesList.count()).toBe(1);
+
+    query.clear();
+    query.sendKeys('date');
+    expect(availableFeaturesList.count()).toBe(2);
   });
 
 });
