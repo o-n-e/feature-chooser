b5426b26c6f3f568515cf6f49e51cf4cdd4b5f0e
o-n-e <stewart.barron@mac.com>
2015-12-16 18:59:07 +0000

    6. There is now a list of features loaded from the server.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,11 +4,12 @@
 
 var featureChooserApp = angular.module('featureChooserApp', []);
 
-featureChooserApp.controller('FeatureCtrl', function($scope, $http) {
+featureChooserApp.controller('FeatureCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $http.get('data/simple-features.json').success(function(data) {
         $scope.availibleFeatures = data;
     });
 
     $scope.orderProp = 'id';
-});
+
+}]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,18 +4,25 @@
 describe ('FeatureChooser controllers', function(){
 
   describe('FeatureCtrl', function(){
-    var scope, ctrl;
+    var scope, ctrl, $httpBackend;
 
     beforeEach(module('featureChooserApp'));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('data/simple-features.json').
+      respond([{name: 'size'}, {name: 'calcDate'}]);
 
-    beforeEach(inject(function($controller) {
-      scope = {};
-      ctrl = $controller('FeatureCtrl', {$scope:scope});
+      scope = $rootScope.$new();
+      ctrl = $controller('FeatureCtrl', {$scope: scope});
     }));
 
-    it('should create "availibleFeatures" model with 6 features', inject(function($controller) {
-      expect(scope.availibleFeatures.length).toBe(6);
-    }));
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.availibleFeatures).toEqual([{name: 'size'},
+        {name: 'calcDate'}]);
+    });
 
     it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('id');
@@ -23,3 +30,30 @@ describe ('FeatureChooser controllers', function(){
 
   });
 });
+
+/*
+ beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+ $httpBackend = _$httpBackend_;
+ $httpBackend.expectGET('phones/phones.json').
+ respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+ scope = $rootScope.$new();
+ ctrl = $controller('PhoneListCtrl', {$scope: scope});
+ }));
+
+
+ it('should create "phones" model with 2 phones fetched from xhr', function() {
+ expect(scope.phones).toBeUndefined();
+ $httpBackend.flush();
+
+ expect(scope.phones).toEqual([{name: 'Nexus S'},
+ {name: 'Motorola DROID'}]);
+ });
+
+
+ it('should set the default value of orderProp model', function() {
+ expect(scope.orderProp).toBe('age');
+ });
+ });
+ });
+ */
