9aaa6db386dd8c159feb2c651158b56a96a37f18
o-n-e <stewart.barron@mac.com>
2015-12-18 11:56:16 +0000

    8. Make the page work without the list

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,45 +10,37 @@ describe('featureChooser', function() {
       browser.get('app/index.html');
     });
 
+
     it('should filter the available features list as a user types into the search box', function() {
 
-      var availableFeaturesList = element.all(by.repeater('feature in availibleFeatures'));
+      var allOptions = element.all(by.options('feature as feature.displayName for feature in availibleFeatures | filter:query | orderBy:orderProp'));
+      expect(allOptions.count()).toEqual(10);
+
+      var firstOption = allOptions.first();
+      expect(firstOption.getText()).toEqual('Calculation Date');
+
       var query = element(by.model('query'));
+      query.sendKeys('p');
 
-      expect(availableFeaturesList.count()).toBe(10);
+      expect(allOptions.count()).toEqual(1);
+      expect(firstOption.getText()).toEqual('Price');
 
-      query.sendKeys('col');
-      expect(availableFeaturesList.count()).toBe(2);
-
-      query.clear();
-      query.sendKeys('date');
-      expect(availableFeaturesList.count()).toBe(2);
     });
 
     it('should be possible to control feature order via the drop down select box', function() {
 
-      var featureNameColumn = element.all(by.repeater('feature in availibleFeatures').column('displayName'));
+      var allOptions = element.all(by.options('feature as feature.displayName for feature in availibleFeatures | filter:query | orderBy:orderProp'));
+
       var query = element(by.model('query'));
+      query.sendKeys('s')
 
-      function getNames() {
-        return featureNameColumn.map(function(elm) {
-          return elm.getText();
-        });
-      }
-
-      query.sendKeys('s'); //let's narrow the dataset to make the test assertions shorter
-
-      expect(getNames()).toEqual([
-        "Symbol",
-        "Size"
-      ]);
+      var firstOption = allOptions.first();
+      expect(firstOption.getText()).toEqual('Symbol');
 
       element(by.model('orderProp')).element(by.css('option[value="displayName"]')).click();
 
-      expect(getNames()).toEqual([
-        "Size",
-        "Symbol"
-      ]);
+      firstOption = allOptions.first();
+      expect(firstOption.getText()).toEqual('Size');
 
     });
 
